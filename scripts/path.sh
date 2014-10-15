85a335f727485dba6cb1f3f5150d5e88a10b2d7b
Diego Nogueira <diegoalvareznogueira@gmail.com>
2014-10-14 21:51:33 -0300

    added GOPATH

--- scripts/path.sh
+++ scripts/path.sh
@@ -3,7 +3,12 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 		export PATH="$HOME/.rbenv/shims:$PATH"
 		export PATH="$PATH:$HOME/bin"
 		export PATH="$PATH:/usr/local/heroku/bin"
+
+		# golang
+		export GOPATH=$HOME/go
+		export PATH=$PATH:$GOPATH/bin
 		export PATH="$PATH:/usr/local/go/bin"
+
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 		export DYLD_LIBRARY_PATH="/usr/local/mysql/lib:$DYLD_LIBRARY_PATH"
 		export PATH="$PATH:/bin"
@@ -17,11 +22,16 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 		export PATH="$PATH:/opt/X11/bin"
 		export PATH="$PATH:/usr/local/git/bin"
 		export PATH="$PATH:/usr/local/mysql/bin"
-		export PATH="$PATH:/usr/local/go/bin"
 		export PATH="$PATH:$HOME/Documents/scala/play-2.2.3"
 		export PATH="$PATH:$HOME/Documents/scala/scala-2.10.4/bin"
 		export PATH="$PATH:$HOME/Projects/applications/activator-1.2.1-minimal"
 		export PATH="$PATH:$HOME/bin"
 		export PATH="$PATH:$HOME/.rbenv/shims"
 		export PATH="$PATH:$HOME/bin"
+
+		# golang
+		export GOPATH=$HOME/go
+		export PATH=$PATH:$GOPATH/bin
+		export PATH="$PATH:/usr/local/go/bin"
+
 fi
