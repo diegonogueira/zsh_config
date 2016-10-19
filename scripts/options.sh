051ab748d41297e5dccdf6d53cc4a1670f6f7125
Diego Nogueira <diegoalvareznogueira@gmail.com>
2016-10-19 08:55:51 -0200

    Added asdf

--- scripts/options.sh
+++ scripts/options.sh
@@ -1,3 +1,7 @@
 unset MAILCHECK # disable "you have mail" warning
 
 if which rbenv > /dev/null; then eval "$(rbenv init - zsh)"; fi
+
+# asdf
+. $HOME/.asdf/asdf.sh
+. $HOME/.asdf/completions/asdf.bash
