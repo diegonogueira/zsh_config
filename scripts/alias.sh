13fd94f1a15c619516b07aeea33576114d19b61e
Diego Nogueira <diegoalvareznogueira@gmail.com>
2016-11-05 10:20:19 -0200

    created some git alias

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -1,30 +1,35 @@
+# Shell
+
+alias n="nvim"
 alias ll="ls -Glahs"
 alias ls="/bin/ls -G"
 alias psgrep="ps aux | egrep -v egrep | egrep"
 alias internal_ip="ipconfig getifaddr en0"
 alias external_ip="curl -s http://checkip.dyndns.org | sed 's/[a-zA-Z/<> :]//g'"
-alias be="bundle exec"
+
+# GIT
+
 alias gs="git status"
-alias gb="git branch"
-alias gba="git branch -a"
-alias gm="git merge --no-ff"
-alias gpm="git push origin master"
-alias gf="git fetch origin"
+alias gp="git push"
+alias gf="git fetch origin -p"
 alias gl="git log --graph"
-alias gsl="git stash list"
-alias rt="rake -Itest"
+alias gpr="git pull --rebase"
+alias gfs="git flow feature start "
+alias gfp="git flow feature publish "
+alias gff="git flow feature finish "
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     alias ga="gitk --all"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     alias ga="gitx --all"
     alias vim='/Applications/MacVim.app/Contents/MacOS/Vim'
+    alias vi='/Applications/MacVim.app/Contents/MacOS/Vim'
 
     # Docker
     alias dk="docker"
     alias dkm="docker-machine"
     alias dkc="docker-compose"
-    alias dkdown="dkm stop docker"
+    alias dkdown="dkm stop docker-vb"
     alias dkup="dkc up"
     alias dkrun="dkc run --rm web"
     alias dkbash="dkc run --service-ports --rm web bash"
