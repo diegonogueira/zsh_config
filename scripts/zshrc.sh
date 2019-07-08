d5bc4fb5944e87c3d8c319cdde4e4dba18323640
Diego Nogueira <diegoalvareznogueira@gmail.com>
2019-07-08 00:00:31 -0300

    removed zsh-syntax-highlighting

--- scripts/zshrc.sh
+++ scripts/zshrc.sh
@@ -53,7 +53,7 @@ export ZSH=$HOME/.oh-my-zsh
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 #plugins=(common-aliases zsh-history-substring-search vi-mode)
-plugins=(common-aliases vi-mode zsh-syntax-highlighting history-substring-search)
+plugins=(common-aliases vi-mode history-substring-search)
 
 source $ZSH/oh-my-zsh.sh
 
